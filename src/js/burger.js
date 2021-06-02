2c9f83472b2a255bd25cd9069f6f7527c679cc4e
denis <ovicartel@gmail.com>
2021-06-02 18:43:13 +0300

    @media (max-width: 991px)  .header is completed, burger is making

--- src/js/burger.js
+++ src/js/burger.js
@@ -2,22 +2,31 @@
 console.log('ksjdks');
 
 
-const menu = document.querySelector('.header__menu'),
-    menuItem = document.querySelectorAll('.header__menu-item'),
-    hamburger = document.querySelector('.burger');
+// const menu = document.querySelector('.header__menu'),
+//     menuItem = document.querySelectorAll('.header__menu-item'),
+//     hamburger = document.querySelector('.header__burger');
 
-hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('burger_active');
-    menu.classList.toggle('header__menu_active');
-});
 
-menuItem.forEach(item => {
-    item.addEventListener('click', () => {
-        hamburger.classList.toggle('burger_active');
-        menu.classList.toggle('header__menu_active');
+// hamburger.addEventListener('click', () => {
+//     hamburger.classList.toggle('header__burger_active');
+//     menu.classList.toggle('header__menu_active');
+
+
+// });
+
+// menuItem.forEach(item => {
+//     item.addEventListener('click', () => {
+//         hamburger.classList.toggle('header__burger_active');
+//         menu.classList.toggle('header__menu_active');
+//     });
+// });
+
+
+$(document).ready(function () {
+    $('.header__burger').click(function (event) {
+        $('.header__burger, .header__menu').toggle('active');
+        $('body').toggleClass('lock');
     });
 });
-
-
 
 
