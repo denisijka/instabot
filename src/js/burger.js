6279c83de1957fa475d64b3b4c0faace68a7d609
denis <ovicartel@gmail.com>
2021-06-07 19:41:35 +0300

    burger menu is completed

--- src/js/burger.js
+++ src/js/burger.js
@@ -6,23 +6,36 @@ const menu = document.querySelector('.header__menu'),
     menuList = document.querySelector('.header__list'),
     menuItem = document.querySelectorAll('.header__menu-item'),
     hamburger = document.querySelector('.header__burger'),
-    newElement = document.createElement('li'),
-    ru = document.querySelector('.header__change-ru'),
-    slash = document.querySelector('.slash'),
-    en = document.querySelector('.header__change-en');
+    newLi = document.createElement('li'),
+    newARu = document.createElement('a'),
+    newSlash = document.createElement('p'),
+    newAEn = document.createElement('a'),
+    newLiMenu = document.createElement('li');
 
-// newElement.classList.add('extra_menu-item');
-// newElement.classList.add('header__menu-item');
-// newElement.append(ru, slash, en);
-// menuList.append(newElement);
+
+
 
 
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('header__burger_active');
     menu.classList.toggle('header__menu_active');
 
-    ru.classList.toggle('on');
+    // create classes
+    newLi.classList.add('header__menu-extra');
+    newLiMenu.classList.add('header__menu-extra');
+    newARu.classList.add('header__rus');
+    newSlash.classList.add('header__slash');
+    newAEn.classList.add('header__eng');
+    // add textContent
+    newLiMenu.textContent = 'Меню';
+    newARu.textContent = 'ru';
+    newSlash.textContent = '/';
+    newAEn.textContent = 'en';
 
+
+    newLi.append(newARu, newSlash, newAEn);
+    menuList.prepend(newLiMenu);
+    menuList.append(newLi);
 
 });
 
