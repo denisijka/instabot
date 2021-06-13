2fd2102a2f61f53ffcd439251a792af29d614549
denis <ovicartel@gmail.com>
2021-06-13 16:08:26 +0300

    tabs: 1. задаем переменные

--- src/js/tabs.js
+++ src/js/tabs.js
@@ -1,5 +1,8 @@
 "use strict";
 console.log('tabs.js');
 window.addEventListener('DOMContentLoaded', () => {
+    const tabs = document.querySelectorAll('.working__tab'),
+        tabsContent = document.querySelectorAll('.working__tabcontent'),
+        tabsParent = document.querySelector('.working__tabs');
 
 });
