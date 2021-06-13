54c82aba6f3b0f823fc29be92e0b00c10efc9a33
denis <ovicartel@gmail.com>
2021-06-13 19:08:55 +0300

    task.4: contains
    Будем при помощи contains определять точно  что мы кликнули в tab.

--- src/js/tabs.js
+++ src/js/tabs.js
@@ -24,5 +24,18 @@ window.addEventListener('DOMContentLoaded', () => {
     tabsParent.addEventListener('click', (event) => {
         const target = event.target;
 
+        if (target && target.classList.contains('working__tab')) {
+            // Когда мы кликнули в определенный пункт (working__tab), мы должны 
+            // определить его номер в списке всех наших табов и по этому
+            // номеру вызвать функцию showTabContent();, для того что бы определенный таб показать на страничке. И сделать это мы можем обычным перебором. То есть мы переберем все табы которые лежат в нашей переменной tabs и будем сравнивать что если элемент который находится в этом tabs совпадает с тем элементом который кликнул пользователь, тогда мы берем его номер и показываем на странице.
+            tabs.forEach((item, i) => {
+                if (target == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+
+
+        }
     });
 });
